e13541e36f7b9364042bbc4f5f1deb62f80ec2c4
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-05-01 21:40:48 +0530

    Implement initial file and folder

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,30 @@
+// import MobileNav from "@/components/MobileNav";
+// import Sidebar from "@/components/Sidebar";
+// import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+//   const loggedIn = await getLoggedInUser();
+
+//   if(!loggedIn) redirect('/sign-in')
+
   return (
-    <main className="flex min-h-screen w-full justify-between font-inter">
-      {children}
-      <div className="auth-asset">
-        <div>
-          <Image 
-            src="/icons/auth-image.svg"
-            alt="Auth image"
-            width={500}
-            height={500}
-            className="rounded-l-xl object-contain"
-          />
-        </div>
-      </div>
+    <main className="flex h-screen w-full font-inter">
+      {/* <Sidebar user={loggedIn} /> */}
+      {/* <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div> */}
+            {/* <MobileNav user={loggedIn} /> */}
+          {/* </div>
+        </div> */}
+        {children}
+      {/* </div> */}
     </main>
   );
 }
