71f85f1a4e9dc1ae2b1e1bf1b4f1b3e2aa204087
Arkadeep Bag <arkadeepbag@Arkadeeps-MacBook-Air.local>
2025-05-05 21:36:18 +0530

    Implement the basi layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import React from 'react'
 
 const Home = () => {
+    const loggedIn={firstName:'Arkadeep',lastName:'Bag',email:'arkadeepbag3390@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox 
+                type='greeting'
+                title='Welcome'
+                user={loggedIn?.firstName||'Guest'}
+                subtext='Access and manage your account and transactions efficiently'></HeaderBox></header>
+
+           RECENT TRANSACTIONS  
+        </div>
+        <RightSidebar
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance:125.50},{currentBalance:250.30}]}
+        ></RightSidebar>
+    </section>
   )
 }
 
